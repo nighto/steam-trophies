fb6158bdcc575121a2756de8f7a06c1324d64a59
Arlindo Pereira <arlindosaraivapereira@gmail.com>
2025-04-04 15:02:46 +0200

    hide unplayed games and games without achievements

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ const URL_TROPHIES = 'http://localhost:3000/trophies';
 // another option is https://cdn.cloudflare.steamstatic.com/steam/apps/1569040/capsule_231x87.jpg
 const STEAM_GAME_IMAGE = 'https://steamcdn-a.akamaihd.net/steam/apps';
 
+// TODO move this to be configurable by the user
+const OPTIONS = {
+  SHOW_GAMES_NOT_PLAYED: false,
+  SHOW_GAMES_WITHOUT_TROPHIES: false,
+};
+
 // TODO unify those .then calls
 const fetchGames = fetch(URL_GAMES)
   .then(response => {
@@ -24,6 +30,15 @@ const fetchTrophies = gameId => fetch(`${URL_TROPHIES}/${gameId}`)
     return response.json();
   });
 
+const filterByOptions = game => {
+  if (!OPTIONS.SHOW_GAMES_NOT_PLAYED) {
+    if (game.playtime_forever === 0 || game.rtime_last_played === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
 const showGamesList = data => {
   // remove loading
   document.querySelector('#loading').remove();
@@ -34,16 +49,17 @@ const showGamesList = data => {
 
   // append game list
   const gamesList = document.querySelector('#games-list');
-  sortedGames.forEach(game => gamesList.appendChild(gameListEntry(game)));
+  sortedGames.filter(filterByOptions).forEach(game => gamesList.appendChild(gameListEntry(game)));
 
   // keep chainable
   return data;
 };
 
 const gameListEntry = game => {
-  // game.appid, .name, .img_icon_url, .rtime_last_played
+  // game.appid, .name, .img_icon_url, .rtime_last_played, .playtime_forever
   const div = document.createElement('div');
   div.className = 'games-list__game';
+  div.id = `game-${game.appid}`;
   div.tabIndex = 0;
 
   // game image
@@ -70,10 +86,10 @@ const gameListEntry = game => {
   trophyCount.id = `trophies-${game.appid}`;
   trophyCount.className = 'games-list__game__trophies';
   div.appendChild(trophyCount);
-  
+
   // fetch trophies
   fetchTrophies(game.appid).then(data => updateTrophies(data, game.appid));
-  
+
   return div;
 };
 
@@ -82,27 +98,33 @@ const updateTrophies = (data, appid) => {
   
   // get trophies count
   const { playerstats: { achievements } } = data;
+
+  // remove games without achievements
+  if (!OPTIONS.SHOW_GAMES_WITHOUT_TROPHIES && achievements === undefined) {
+    return document.querySelector(`#game-${appid}`).remove();
+  }
+
   const totalAchievementsCount = achievements.length;
   const unlockedAchievementsCount = achievements.filter(a => a.achieved === 1).length;
   const percentage = Math.round((unlockedAchievementsCount / totalAchievementsCount) * 100);
   // fake platinum trophy = if has all achievements and at least one, aka. completed the game
   const platinum = totalAchievementsCount > 0 && unlockedAchievementsCount === totalAchievementsCount ? 1 : 0;
-  
+
   // create progressbar
   const progress = document.createElement('progress');
   progress.max = totalAchievementsCount;
   progress.value = unlockedAchievementsCount;
-  
+
   // create percentage value
   const label = document.createElement('label');
   label.textContent = `${percentage}%`;
-  
+
   // create trophies
   const platinumTrophy = document.createElement('span');
   platinumTrophy.textContent = platinum;
   const regularTrophies = document.createElement('span');
   regularTrophies.textContent = unlockedAchievementsCount;
-  
+
   // append them
   const progressDiv = document.querySelector(`#progress-${appid}`);
   const trophiesDiv = document.querySelector(`#trophies-${appid}`);
